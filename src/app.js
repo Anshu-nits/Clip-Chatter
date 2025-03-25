3716f90111146ec64b472a99f4a28ad76a89ec64
Anshu-nits <anshurajjaiswal2003@gmail.com>
2025-03-25 23:41:50 +0530

    added user and video models

--- src/app.js
+++ src/app.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}))
+}))        
 
 app.use(express.json({limit: "16kb"})) 
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser)
 
-export { app }
+export { app }  
