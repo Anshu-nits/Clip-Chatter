b90fe7d267c231b8eb25d723d41dcafc3597f15c
Anshu-nits <anshurajjaiswal2003@gmail.com>
2025-04-16 17:12:40 +0530

    added refreshAccessToken endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,6 +6,7 @@ import { User } from "../models/user.model.js"
 export const verifyJWT = asyncHandler( async (req, _, next) =>{
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        // console.log(token)
         
         if(!token){
             throw new ApiError(401, "Unauthorized request")
@@ -13,15 +14,15 @@ export const verifyJWT = asyncHandler( async (req, _, next) =>{
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
-        const user = await User.findById(decodedToken._id).select("-password -refreshToken")
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
         if(!user){
-            throw new ApiError(401, "Invalid Access Token")
+            throw new ApiError(401, "Invalid Access Token hello")
         }
         
         req.user = user;
-        next()
+        next() 
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token")
+        throw new ApiError(401, error?.message || "Invalid access token am bro")
     }
 })
